d25d5f9cff010a4c85c30418eb94d38496a61f0c
emmagency <kebohemmagency@gmail.com>
2016-12-29 08:38:43 +0000

    Added test directory.

---
+++ test/org/backend/mdxmaps/Services/SBSLFactoryServiceTest.java
@@ -0,0 +1,15 @@
+package org.backend.mdxmaps.Services;
+
+import org.junit.Test;
+
+/**
+ * Created by Emmanuel Keboh on 28/12/2016.
+ */
+public class SBSLFactoryServiceTest {
+
+    @Test
+    public ResponseService getRoute() {
+        return null;
+    }
+
+}
